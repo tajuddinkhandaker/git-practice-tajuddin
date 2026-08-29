e41de8f81c5527f5834643b8debe3223b78127e9
Tajuddin Khandaker <tajuddin.khandaker.cse.ju@gmail.com>
2026-08-29 22:19:01 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from datetime import date
 
+from utils import add, subtract
+
 # Print name
 print("Khandaker Saleh Mohammad Tajuddin")
 
 # Print today's date
 today = date.today()
 print(today)
+
+# Call add and subtract from utils
+a = 10
+b = 4
+print(f"add({a}, {b}) = {add(a, b)}")
+print(f"subtract({a}, {b}) = {subtract(a, b)}")
